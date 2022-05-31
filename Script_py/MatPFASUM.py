f65de937fa586c557f1ca48172bc983677b89ddb
clarpalmtree <claraa.toussaint@gmail.com>
2022-05-31 11:58:18 +0200

    Calcule freqAA, FreqCouple + Matrice (fin)

--- Script_py/MatPFASUM.py
+++ Script_py/MatPFASUM.py
@@ -18,39 +18,65 @@ dAmbigu = {'Z': { 'tab' :['I', 'L'], 'valeur' : 1/2 }, 'J' : { 'tab': ['Q' , 'E'
 
 ## ..................................................................................................................................................
 
+
+
 ### FONCTION CALCULE DE FREQUENCE D'AA...............................................................................................................
 #....................................................................................................................................................
-def FreqAA( liSeqAli ):
+def FreqAA( dFreqAA, liSeqAli, tab_couple ):
     """
         input : une liste de tuple (seq, nom), un dico vide
-        output : un de la forme : { A : 0, T : 0, D : 0, ...}
+        output : un dico de la forme : { A : 0, T : 0, D : 0, ...}
     """
 
     dico_occ = {}
-    tab_couple = []
     for aa in liste_aa_ambigu :
         dico_occ[aa]={}
         for aaa in liste_aa_ambigu :
             dico_occ[aa][aaa] =0
 
+    #En effet il existe des fichiers avec un seul cluster 
+    #donc j'ai décidé de mettre les numéros de cluster dans une liste
+    #et si la taille du tableau est > 1 on caclule entre cluster
+    #sinon on calcule dans le cluster puisqu'on en a qu'un
+
+    #Je ne voyais pas comment d'abord avoir tous les numéros de cluster
+    #pour ensuite calculer entre cluster si il existe pls cluster
+    #pour moi je devais forcémement d'abord parcourir le fichier puis
+    # ajouter les numéros de clusters si ils sont différents
+    #puis reparcourir et calculer
+
+    tab_numclust = []
+    for i in range(len(liSeqAli)):
+        name1, seq1 = liSeqAli[i]
+        cluster_name_1 = int ( name1.split()[0] )
+        if cluster_name_1 not in tab_numclust : 
+            tab_numclust.append(cluster_name_1)
+
+
     for i in range(len(liSeqAli)):
         name1, seq1 = liSeqAli[i]
         cluster_name_1 = int ( name1.split()[0] )
         taille_cluster1 = int( name1.split()[1] )
+            
 
         for j in range(i+1, len(liSeqAli)):
             name2, seq2 = liSeqAli[j]
             cluster_name_2 = int ( name2.split()[0] )         
             taille_cluster2 = int( name2.split()[1] )
+            
+            if len(tab_numclust) > 1 :
+                #on calcule les pairs entre les clusters
+                if cluster_name_2 != cluster_name_1 : 
+                    for (aa1, aa2) in zip(seq1, seq2):
+                        if aa1 in liste_aa_ambigu and aa2 in liste_aa_ambigu :
+                            dico_occ[aa1][aa2] +=  ( 1 / taille_cluster1 ) * ( 1 / taille_cluster2 )
 
-            #on calcule les pairs entre les clusters
-            if cluster_name_2 != cluster_name_1 : 
+            else :
                 for (aa1, aa2) in zip(seq1, seq2):
-                    if aa1 in liste_aa_ambigu and aa2 in liste_aa_ambigu :
-                        dico_occ[aa1][aa2] +=  ( 1 / taille_cluster1 ) * ( 1 / taille_cluster2 )
-                        
-                    
+                        if aa1 in liste_aa_ambigu and aa2 in liste_aa_ambigu :
+                            dico_occ[aa1][aa2] +=  ( 1 / taille_cluster1 ) * ( 1 / taille_cluster2 )
 
+                   
     # on redispatch les aa ambigu  
     # selon si on a au moins un aa ambigu dans notre pair
     # ou si les deux aa sont ambigu
@@ -81,17 +107,19 @@ def FreqAA( liSeqAli ):
             if (aa1 in liste_aa and aa2 in liste_aa) and (dico_occ[aa1][aa2] != 0.0):
                 tab_couple.append( { "couple": aa1 + aa2, "aa1": aa1, "aa2" : aa2 })
 
-   
+    
     # Après avori redispatché je supprime les aa ambigu de mon dico
     [dico_occ.pop(key, None) for key in ['X', 'Z', 'J', 'B']]
     for ele in dico_occ :
         [dico_occ[ele].pop(key, None) for key in ['X', 'Z', 'J', 'B']]
 
     # Enfin je calcule la freq des aa 
-    dico_freq = {}
+    
     somme_all = 0
+    dico_freq = {}
     for aa in liste_aa :
-        dico_freq[aa] = 0
+        dico_freq[aa]= 0
+    
 
     for a in dico_occ :
         for aa in dico_occ[a] :
@@ -109,6 +137,8 @@ def FreqAA( liSeqAli ):
     #On divise par le nombre de pair totaux
     for ele in dico_freq :
         dico_freq[ele] = dico_freq[ele] / somme_all
+        #ajout des fréquences dans le dico
+        dFreqAA[ele] += dico_freq[ele]
     
     #vérification
     """
@@ -117,13 +147,42 @@ def FreqAA( liSeqAli ):
         tot +=dico_freq[ele]
     print("la somme est =", tot)
     """
+    #print(tab_couple)
+
 
     return dico_freq, tab_couple
+
+### FONCTION QUI RENVOIE UN DICO DES FRÉQUENCES D'AA DE TOUS LES FICHIERS CONTENUT DANS UN DOSSIER...................................................
+#....................................................................................................................................................
+def MultiFreq(directory) :
+    """
+        input : le chemin dans lequel se trouve tous les fichiers dont on veut les fréquences
+        output : un dico de fréquence d'AA  : { A : 0, T : 0, D : 0, ...} et un tableau
+                contenant tous les couples 
+    """
+
+    dFreqAA = {}
+    for aa in liste_aa :
+        dFreqAA[aa] = 0
+
+    tab_couple = []
+    tot = 0
+    for files in directory :
+        seq = readFastaMul(files)
+        dFreq, Tcouple= FreqAA(dFreqAA, seq, tab_couple)
+        tot +=1
+
+
+    for ele in dFreqAA :
+        dFreqAA[ele] = dFreqAA[ele] /tot
+
+    return dFreqAA, tab_couple
+
 
 
 ### FONCTION CALCULE DES FRÉQUENCES DES PAIRS D'AA...................................................................................................
 #....................................................................................................................................................
-def FreqCoupleAA (dico_freq , tab_couple) :
+def FreqCoupleAA (dico_freq , tab) :
     """
         input : une liste de tuple (nom, seq) et un dico vide
         output : un dico de la fréquence des pairs d'AA de la forme : 
@@ -137,6 +196,9 @@ def FreqCoupleAA (dico_freq , tab_couple) :
             dFreqCouple[aa][aaa] = 0
 
 
+    #on supprime les doublons dans la liste
+    #sinon je vais compter plusieurs fois le même couple
+  
     #cette partie va sans doute être modifié car elle peut s'écrire juste ainsi
     """
     for couple in tab_couple :
@@ -150,22 +212,24 @@ def FreqCoupleAA (dico_freq , tab_couple) :
     # {A : {T: 2, C:0}, T: {A :0, C:0}}
     #mais moi je le veux ainsi : {A : {T: 1, C:0}, T: {A :1, C:0}}
     #donc je fais le code ci-dessous
+    tab_couple = []
+    for couple in tab:
+        if couple not in tab_couple : 
+            if couple["aa1"] ==  couple["aa2"] :
+                dFreqCouple[ couple["aa1"] ][ couple["aa2"] ] = 2*( dico_freq [ couple["aa1"] ] * dico_freq[ couple["aa2"] ] )
+                tab_couple.append(couple)
 
-    for couple in tab_couple :
-        if couple["aa1"] ==  couple["aa2"] :
-            dFreqCouple[ couple["aa1"] ][ couple["aa2"] ] = 2*( dico_freq [ couple["aa1"] ] * dico_freq[ couple["aa2"] ] )
-
-        else : 
-            dFreqCouple[ couple["aa1"] ][ couple["aa2"] ] =  dico_freq [ couple["aa1"] ] * dico_freq[ couple["aa2"] ] 
-            dFreqCouple[ couple["aa2"] ][ couple["aa1"] ] =  dico_freq [ couple["aa1"] ] * dico_freq[ couple["aa2"] ] 
-
+            else : 
+                dFreqCouple[ couple["aa1"] ][ couple["aa2"] ] =  dico_freq [ couple["aa1"] ] * dico_freq[ couple["aa2"] ] 
+                dFreqCouple[ couple["aa2"] ][ couple["aa1"] ] =  dico_freq [ couple["aa1"] ] * dico_freq[ couple["aa2"] ] 
+                tab_couple.append(couple)
 
     #si je ne fais pas ça ma matrice ne va pas faire prendre en compte TA 
     # et je trouve ça bizarre
     # même si ça doit revenir au même
 
-    #print(dFreqCouple)
 
+    #print(dFreqCouple)
     return dFreqCouple
 
 
@@ -213,9 +277,22 @@ file2 =  main_path + dossier + file_two
 
 
 #liSeqAli = readFastaMul(file1)
-liSeqAli = readFastaMul(file2)
+#liSeqAli = readFastaMul(file1)
 
+
+"""
 dFreqAA, tab_couple = FreqAA(liSeqAli)
 dFreqCouple = FreqCoupleAA(dFreqAA, tab_couple)
 print(computeMatrixPFASUM(dFreqAA, dFreqCouple, 1))
+"""
+
+directory = "/home/ctoussaint/Stage_MNHN/test/multi"
+directory = Path(directory)
+directory = directory.iterdir()
+
+
+dFreqAA, tab = MultiFreq(directory)
+dFreqCouple = FreqCoupleAA(dFreqAA, tab)
+print(computeMatrixPFASUM(dFreqAA, dFreqCouple, 1))
+
 
